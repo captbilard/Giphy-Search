d0431f35ecd033ad7aa631ae16210e616a1c185d
bilard <bilardosinulu@gmail.com>
2019-02-04 22:28:17 +0100

    Updated the jquery logic, now it can fetch the giphy api and return an object

---
+++ README.md
@@ -0,0 +1,2 @@
+# Giphy-Search
+This is a web project for ALC 3.1 intermediate Web developers. The giphy searchis a web app designed with the bootstrap framework and jquery, Users enter the a search item and gifs matching such item is returned in the 6 spaces available. Gif's are gotten from Giphy.
---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready(() => {
+  //once the DOM is done loading save the api key
+  let api_key = "8s65PwK5uN7OqwCC9doyr2CJd8XU7Uv4";
+  //listen for the click event on the search button
+  $("#button-addon2").click(() => {
+    //get the value from the search bar and save it in query and also save the url
+    let query = $("#query").val();
+    let xhr = `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${api_key}&limit=6`;
+    //get the data using the fetch api
+    fetch(xhr).then((response) => {
+      return response.json()
+    }).then((myjson) => {
+      //let results = JSON.stringify(myjson);
+      //load the data into each of the div
+      //Have been able to loop through the gif and get the url to display in the selected box. now i need to get the picture to appear now instead of url
+      myjson.data.forEach((gif) => {
+        console.log(gif.images.downsized_medium.url);
+        let e = gif.images.downsized_medium.url;
+        console.log(e);
+        $(".col-sm1").html(e);
+        //console.log(gif);
+      })
+    })
+  })
+});
