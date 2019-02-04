e00bf065f70a53e464cfd2167677f14095ea1a13
Adebayo Osinulu <33939983+captbilard@users.noreply.github.com>
2019-02-04 23:41:38 +0100

    Update main.js

--- main.js
+++ main.js
@@ -23,19 +23,4 @@ $(document).ready(() => {
       })
     })
 =======
-  let api_key = "8s65PwK5uN7OqwCC9doyr2CJd8XU7Uv4";
-  $("#button-addon2").click(() => {
-    let query = $("#query").val();
-    let url = `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${api_key}`;
-    fetch(url).then((response) => {
-      return response.json()
-    }).then(myjson => {
-      return JSON.stringify(myjson);
-    }).then(result => {
-      for (let i = 0; i<=6; i++){
-        
-      }
-    });
->>>>>>> dc26108ffb7ba7988893d51be75467f2ec70c111
-  })
-});
+
